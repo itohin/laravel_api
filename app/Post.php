a9926135469e92208dee167199a8b64d0df1ae3e
itohin <itoxa@MacBook-Pro.local>
2019-02-20 10:29:31 +0300

    Posts, Topics relations

--- app/Post.php
+++ app/Post.php
@@ -6,5 +6,15 @@ use Illuminate\Database\Eloquent\Model;
 
 class Post extends Model
 {
-    //
+    protected $fillable = ['body'];
+
+    public function topic()
+    {
+        return $this->belongsTo(Topic::class);
+    }
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
 }
