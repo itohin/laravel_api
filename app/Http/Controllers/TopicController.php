96760b173e227b323fd15ede312d69a390ed3da0
itohin <itoxa@MacBook-Pro.local>
2019-02-20 11:05:32 +0300

    Store posts and topics

--- app/Http/Controllers/TopicController.php
+++ app/Http/Controllers/TopicController.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Controllers;
 
+use App\Post;
+use App\Topic;
 use Illuminate\Http\Request;
 use App\Http\Requests\StoreTopicRequest;
 
@@ -9,6 +11,15 @@ class TopicController extends Controller
 {
     public function store(StoreTopicRequest $request)
     {
+        $topic = new Topic;
+        $topic->title = $request->title;
+        $topic->user()->associate($request->user());
 
+        $post = new Post;
+        $post->body = $request->body;
+        $post->user()->associate($request->user());
+
+        $topic->save();
+        $topic->posts()->save($post);
     }
 }
