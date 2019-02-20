0119ca336e4c231ddd36f86af18b1a17ba6b065b
itohin <itoxa@MacBook-Pro.local>
2019-02-20 08:34:17 +0300

    Basic setup

---
+++ app/Http/Controllers/RegisterController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\User;
+use Illuminate\Http\Request;
+use App\Http\Requests\StoreUserRequest;
+
+class RegisterController extends Controller
+{
+    public function register(StoreUserRequest $request)
+    {
+
+    }
+}
---
+++ app/Transformers/UserTransformer.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Transformers;
+
+
+use App\User;
+
+class UserTransformer extends \League\Fractal\TransformerAbstract
+{
+
+}
